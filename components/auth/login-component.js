f875fcf4c01b035c8712fc9b68420c21b6b8c4c4
Alex Armendariz <115047842+AlexArmendariz03@users.noreply.github.com>
2024-09-04 14:46:38 -0600

    error correction

--- components/auth/login-component.js
+++ components/auth/login-component.js
@@ -1,32 +1,38 @@
-import { Button, Col, Form, Input, Row, Image } from "antd";
+import { Button, Col, Form, Input, Row, Image } from "antd"
 
 const LoginComponent = () => {
   return(
-    <Row justify="center" align="middle" className="login-container">
-      <Col xs={24} sm={18} md={12} lg={8}>
+    <Row
+      justify="center" align="middle"
+      className="login-container">
+      <Col
+        xs={24} sm={18}
+        md={12} lg={8}>
         <Row justify="center">
+          {/* eslint-disable-next-line jsx-a11y/alt-text */}
           <Image
             width={450}
             height={450}
             src="/1.png"
-            preview={false}
-          />
+            preview={false} />
         </Row>
         <Form layout="vertical">
           <Form.Item
             name="username"
-            rules={[{ required: true, message: 'Please input your username!' }]}>
+            rules={[{ required: true, message: "Please input your username!" }]}>
             <Input
-              placeholder="Nombre de usuario"/>
+              placeholder="Nombre de usuario" />
           </Form.Item>
           <Form.Item
             name="password"
-            rules={[{ required: true, message: 'Please input your password!' }]}>
+            rules={[{ required: true, message: "Please input your password!" }]}>
             <Input.Password
-              placeholder="Introduce tu contraseña"/>
+              placeholder="Introduce tu contraseña" />
           </Form.Item>
-          <Form.Item >
-            <Button type="primary" htmlType="submit" block>
+          <Form.Item>
+            <Button
+              type="primary" htmlType="submit"
+              block>
               Login
             </Button>
           </Form.Item>
