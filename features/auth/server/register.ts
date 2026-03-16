39e82202e282e4a46fe72dd1ed418d0f34672b3b
Alex Armendariz <115047842+AlexArmendariz03@users.noreply.github.com>
2026-03-16 00:04:48 -0600

    hardcode register.ts

--- features/auth/server/register.ts
+++ features/auth/server/register.ts
@@ -1,28 +1,48 @@
-import { prisma } from "@/shared/server/prisma"
-import { hashPassword } from "@/shared/server/password"
-
 type RegisterParams = {
-  username: string
-  password: string
-}
+  username: string;
+  password: string;
+};
 
-export async function registerUser({ username, password }: RegisterParams): Promise<{ message: string; status: number }> {
-  const existingUser = await prisma.user.findUnique({
-    where: { username }
-  })
+type StaticUser = {
+  username: string;
+  password: string;
+  role: "leader" | "musico";
+};
+
+const STATIC_USERS: StaticUser[] = [
+  {
+    username: "leader",
+    password: "leader123",
+    role: "leader",
+  },
+  {
+    username: "musico",
+    password: "musico123",
+    role: "musico",
+  },
+];
+
+export async function registerUser({
+                                     username,
+                                     password,
+                                   }: RegisterParams): Promise<{ message: string; status: number }> {
+  const existingUser = STATIC_USERS.find((user) => user.username === username);
 
   if (existingUser) {
-    return { status: 409, message: "El nombre de usuario ya está en uso" }
+    return {
+      status: 409,
+      message: "El nombre de usuario ya está en uso",
+    };
   }
 
-  const hashedPassword = hashPassword(password)
+  STATIC_USERS.push({
+    username,
+    password,
+    role: "musico",
+  });
 
-  await prisma.user.create({
-    data: {
-      username,
-      password: hashedPassword
-    }
-  })
-
-  return { status: 201, message: "Usuario registrado exitosamente" }
+  return {
+    status: 201,
+    message: "Usuario registrado exitosamente",
+  };
 }
