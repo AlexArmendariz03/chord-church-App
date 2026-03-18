9c7204f7a8a986f54ab62af653c7ca941db29707
Alex Armendariz <115047842+AlexArmendariz03@users.noreply.github.com>
2026-03-17 23:37:35 -0600

    interfaces para uploadSong

---
+++ types/UploadSong/interfaces.ts
@@ -0,0 +1,55 @@
+
+export type AlignType = 'left' | 'center' | 'right';
+
+export type ExtraTextItem = {
+    id: string;
+    type: 'tone';
+    text: string;
+    x: number;
+    y: number;
+    color: string;
+    fontSize: number;
+};
+export type Interfaces = {
+    id: string;
+    type: 'content';
+    text: string;
+    x: number;
+    y: number;
+    width: number;
+    color: string;
+    fontSize: number;
+    align: AlignType;
+};
+
+export type BoardConfig = {
+    boardWidth: number;
+    boardHeight: number;
+    backgroundColor: string;
+    borderColor: string;
+    textColor: string;
+    fontSize: number;
+    padding: number;
+    textAlign: AlignType;
+    zoom: number;
+};
+
+export type SongPayload = {
+    boardConfig: {
+        stageWidth: number;
+        stageHeight: number;
+        boardX: number;
+        boardY: number;
+        boardWidth: number;
+        boardHeight: number;
+        backgroundColor: string;
+        borderColor: string;
+        textColor: string;
+        fontSize: number;
+        padding: number;
+        textAlign: AlignType;
+        zoom: number;
+    };
+    contentBlocks: Interfaces[];
+    extraTexts: ExtraTextItem[];
+};
